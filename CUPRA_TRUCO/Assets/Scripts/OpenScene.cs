eee482d4acf824ba03186e9553f09030ad50dc2b
Fran Guerrero <45202002+FranGV98@users.noreply.github.com>
2022-05-19 21:21:17 +0200

    Screen System created and three apps can open their scene

---
+++ CUPRA_TRUCO/Assets/Scripts/OpenScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenScene : MonoBehaviour
+{
+    public List<GameObject> Screens_List;
+    public void OpenAllApps()
+    {
+        for (int i = 0; i < Screens_List.Count; i++)
+        {
+            Screens_List[i].SetActive(false);
+        }
+        Screens_List[0].SetActive(true);
+    }
+
+    public void OpenMaps()
+    {
+        for(int i = 0; i < Screens_List.Count; i++)
+        {
+            Screens_List[i].SetActive(false);
+        }
+        Screens_List[1].SetActive(true);
+    }
+
+    public void OpenDiscord()
+    {
+        for (int i = 0; i < Screens_List.Count; i++)
+        {
+            Screens_List[i].SetActive(false);
+        }
+        Screens_List[2].SetActive(true);
+    }
+
+}
