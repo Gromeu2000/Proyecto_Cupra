ed939f3984b935c0480208e2056536560b51fc4a
Gerard Romeu Vidal <45124641+Gromeu2000@users.noreply.github.com>
2022-05-22 18:36:32 +0200

    xd

--- CUPRA_TRUCO/Assets/Scripts/PlayPause.cs
+++ CUPRA_TRUCO/Assets/Scripts/PlayPause.cs
@@ -9,13 +9,32 @@ public class PlayPause : MonoBehaviour
     [SerializeField] private Sprite[] buttonSprites;
     [SerializeField] private Image targetButton;
 
-    public void ChangeSprite()
+    public AudioClip song1;
+    private AudioSource audioSource;
+
+    private bool paused1;
+    private bool paused2;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        
+    }
+
+    
+
+public void ChangeSprite()
+
     {
         if(targetButton.sprite == buttonSprites[0])
         {
+           
+
             targetButton.sprite = buttonSprites[1];
             return;
         }
+
         targetButton.sprite = buttonSprites[0];
     }
    
